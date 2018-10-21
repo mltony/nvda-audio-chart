4820beca4065e6debea5ed8d04e59c9eb7b182e7
mltony <anton.malykh+0@gmail.com>
2018-10-21 11:17:37 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# nvda-audio-chart
+# AudioChart addon for NVDA
 AudioChart add-on for NVDA allows you to play excel time series as a continuous sound.
+## Download
+TBD
+
+## Usage
+Select a cell or a column of cells containing time series in Microsoft Excel.
+Keystrokes:
+
+* NVDA+A: Play audio chart.
+* NVDA+A twice quickly: Show calibration dialog window.
+
+## Source code
+Source code is available at <http://github.com/mltony/nvda-audio-chart>.
+
+## Feedback
+If you have any questions or comments, or if you find this addon useful, please don't hesitate to contact me at anton.malykh *at* gmail.com.
---
+++ buildVars.py
@@ -0,0 +1,43 @@
+		# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
+	# add-on Name, internal for nvda
+	"addon_name" : "AudioChart",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary" : _("AudioChart"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description" : _("""AudioChart add-on for NVDA allows you to play excel time series as a continuous sound."""),
+	# version
+	"addon_version" : "0.1dev",
+	# Author(s)
+	"addon_author" : u"Tony Malykh <anton.malykh@gmail.com>",
+	# URL for the add-on documentation support
+	"addon_url" : "https://github.com/mltony/nvda-audio-chart",
+	# Documentation file name
+	"addon_docFileName" : "readme.html",
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = [os.path.join("addon", "GlobalPlugins", "*.py"), ]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
