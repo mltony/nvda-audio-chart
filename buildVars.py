a780485fc2e1e7045ab57471429b10660dddca39
mltony <anton.malykh+0@gmail.com>
2018-10-21 12:50:25 -0700

    Minor change

--- buildVars.py
+++ buildVars.py
@@ -41,3 +41,4 @@ i18nSources = pythonSources + ["buildVars.py"]
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 excludedFiles = []
+ 
